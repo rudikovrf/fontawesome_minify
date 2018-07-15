fdcd9cd8591ceeafa7c697fe77fc29db90e7a300
rudikovrf <rudikovrf@gmail.com>
2018-07-16 01:24:32 +0300

    Added tests for HTMLHandler.

--- test.py
+++ test.py
@@ -3,7 +3,6 @@
 import unittest
 import os
 import re
-
 import shutil
 
 from minify import FAHandler, HTMLHandler, get_undirectories
@@ -66,6 +65,72 @@ class TestGetUndirecrories(unittest.TestCase):
         ]
         output_dirs = ['test_dir2']
         self.assertListEqual(get_undirectories(input_dirs), output_dirs)
+
+
+class TestHTMLHandler(unittest.TestCase):
+    """Class tests HTMLHandler class."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create dirs and files for tests."""
+        create_directories()
+        shutil.copyfile('test_data/f1.html', 'test_dir/1/f1.html')
+        shutil.copyfile('test_data/f2.html', 'test_dir/2/f2.html')
+        shutil.copyfile('test_data/f3.js', 'test_dir/2/f3.js')
+
+    @classmethod
+    def tearDownClass(cls):
+        """Remove dirs and files for tests."""
+        remove_directories()
+
+    def test_get_html_files(self):
+        """Test _get_html_files."""
+        instance = HTMLHandler(['test_dir/2', 'test_dir/1'])
+        instance._get_html_files()
+        self.assertListEqual(
+            instance.html_files,
+            ['test_dir/2/f2.html', 'test_dir/1/f1.html']
+        )
+
+    def test_glue_together(self):
+        """Test _glue_together."""
+        instance = HTMLHandler([])
+        instance.html_files = ['test_dir/2/f2.html', 'test_dir/1/f1.html']
+        instance._glue_together()
+        with open('test_dir/2/f3.js') as f_reader:
+            html = f_reader.read()
+        self.assertEqual(
+            re.sub(r'\s', '', instance.html),
+            re.sub(r'\s', '', html)
+        )
+
+    def test_get_icons(self):
+        """Test _get_icons."""
+        with open('test_data/_get_icons.html') as file_reader:
+            data = file_reader.read()
+        instance = HTMLHandler([])
+        instance.html = data
+        instance._get_icons()
+
+        answer = {
+            'moon',
+            'play',
+            'sun',
+            'star',
+            'stroopwafel'
+        }
+        for i in range(1, 25):
+            answer.add('stroopwafel{}'.format(i))
+
+        self.assertSetEqual(instance.icons, answer)
+
+    def test_get_result(self):
+        """Test get_result."""
+        instance = HTMLHandler(['test_dir/2', 'test_dir/1'])
+        self.assertSetEqual(
+            instance.get_result(),
+            {'times', 'pencil-alt', 'trash-alt', 'check'}
+        )
 
 
 if __name__ == '__main__':
---
+++ test_data/f3.js
@@ -0,0 +1,19 @@
+<button>
+    <i class="fas fa-xs fa-pencil-alt"></i>
+</button>
+<button>
+    <i class="fas fa-sm fa-trash-alt"></i>
+</button>
+<div>
+    <input type="text" name="name">
+    <a href="">name</a>
+</div>
+<div>
+    <textarea name="description">description</textarea>
+</div>
+<button>
+    <i class="fas fa-check"></i>
+</button>
+<button>
+    <i class="fas fa-times"></i>
+</button>
